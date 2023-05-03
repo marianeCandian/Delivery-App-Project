9a187867fcbd3ec6cc9a1e660f66d98630f9d6e4
caio pinho <icandoit2026@gmail.com>
2023-05-03 18:20:11 -0300

    feat- route /login

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const route = require('../routes/UserRoute');
 
 const app = express();
+app.use(express.json());
+app.use('/login', route);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
---
+++ back-end/src/services/UserService.js
@@ -0,0 +1,13 @@
+// const Users = require('../database/models');
+// const md5 = require('md5');
+
+// const findByEmail = async (email) => {
+//     const user = await Users.findOne({ where: { email } });
+//     if (!user) {
+//         return { type: 401, message: 'Invalid email ' };
+//     }
+// };
+
+// const login = async (email, password) => {
+ 
+// };
